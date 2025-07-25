da5f3af7a88ae4c0ce908f4ebe1032488c216d68
HunterLinares <jaensegur@gmail.com>
2025-07-25 20:37:16 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,5 @@
-service-worker.js
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('service-worker.js')
+    .then(() => console.log('✅ Service Worker registrado'))
+    .catch(err => console.error('❌ Error al registrar SW:', err));
+}
